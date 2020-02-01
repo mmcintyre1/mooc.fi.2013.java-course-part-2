ac424eb80248c71d507e6529c467b868affe3cac
Michael <mmcintyre@ebsco.com>
2020-02-01 18:24:02 -0500

    completed exercise 1

--- week7-week7_01.Smileys/src/Smileys.java
+++ week7-week7_01.Smileys/src/Smileys.java
@@ -3,9 +3,39 @@ public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+        printWithSmileys("Method");
+        printWithSmileys("\\:D/");
+        printWithSmileys("87.");
+        printWithSmileys("1");
+        printWithSmileys("0123456789");
+        printWithSmileys("12345678");
+    }
+
+    private static void printWithSmileys(String characterString) {
+        if (isOdd(characterString)) {
+            characterString += " ";
+        }
+        
+        characterString = " " + characterString + " ";
+        
+        int numOfSmileys = (characterString.length() / 2) + 2;
+
+        System.out.println(createSmileyString(numOfSmileys));
+        System.out.println(createSmileyString(1) + characterString + createSmileyString(1));
+        System.out.println(createSmileyString(numOfSmileys));
+    }
+
+    public static String createSmileyString(int howMany) {
+        String smileyString = "";
+
+        for (int i = 0; i < howMany; i++) {
+            smileyString += ":)";
+        }
+        return smileyString;
+    }
+
+    public static boolean isOdd(String characterString) {
+        return characterString.length() % 2 == 1;
     }
 
 }
