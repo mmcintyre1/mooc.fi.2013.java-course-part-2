cf8562d6116a3ea35e3be359d94d68c4b14c2dfa
Michael <mmcintyre@ebsco.com>
2020-02-23 17:08:24 -0500

    completed exercise 10

---
+++ week8-week8_10.NationalService/src/MilitaryService.java
@@ -0,0 +1,27 @@
+
+public class MilitaryService implements NationalService {
+    
+    private int daysLeft;
+
+    public MilitaryService(int daysOfService) {
+        this.daysLeft = daysOfService;
+    }
+
+    @Override
+    public int getDaysLeft() {
+        return this.daysLeft;
+    }
+
+    @Override
+    public void work() {
+        if ( this.daysLeft > 0 ) {
+            this.daysLeft--;
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
